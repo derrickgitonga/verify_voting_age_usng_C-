d719830597abe85f2afba4b3798f29fb346c0850
derrickgitonga <101645445+derrickgitonga@users.noreply.github.com>
2022-03-15 17:16:03 +0300

    Add files via upload

---
+++ voting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age;
+  cout<<"enter your age";
+  cin>>age;
+  if (age <18 )
+  cout<<"cannot vote";
+  else
+  cout<<"can vote";
+  return 0;
+}
